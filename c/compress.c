7660aacb4d2d336ce98572b207d601242d702f73
Taiizor <41683699+Taiizor@users.noreply.github.com>
2026-09-15 06:15:59 +0300

    feat: initial package scaffolding from template

---
+++ c/compress.c
@@ -0,0 +1,5 @@
+#include "compress.h"
+
+int alya_compress_add(int a, int b) {
+    return a + b;
+}
---
+++ c/compress.h
@@ -0,0 +1,6 @@
+#ifndef ALYA_Compress_H
+#define ALYA_Compress_H
+
+int alya_compress_add(int a, int b);
+
+#endif
